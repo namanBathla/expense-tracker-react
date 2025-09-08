cfa14a6cfa292401f2c1df99994d1ee02b666b73
Naman <namanuser1307@gmail.com>
2025-09-09 01:02:59 +0530

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOuM7MEwBzafs66MagYjahQBm3xyktELQ",
+  authDomain: "expense-tracker-react-5a6cc.firebaseapp.com",
+  projectId: "expense-tracker-react-5a6cc",
+  storageBucket: "expense-tracker-react-5a6cc.firebasestorage.app",
+  messagingSenderId: "537740664863",
+  appId: "1:537740664863:web:ee04374b75e25cf8b2b52a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
