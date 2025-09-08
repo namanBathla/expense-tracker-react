a0dafd2b56e8118feb974343f18cb927455ceffd
Naman <namanuser1307@gmail.com>
2025-09-09 01:50:09 +0530

    test firebase write operation

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Expense = () => {
   return (
-    <div></div>
+    <div className='grid grid-cols-5 p-2 text-center'>
+      <div>05 Aug 2025</div>
+      <div>1000</div>
+      <div>Rent for the month of August</div>
+      <div>Fixed Expense</div>
+      <div className='text-red-600 cursor-pointer'>Delete</div>
+    </div>
   )
 }
 
--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import Transaction from '../components/Transaction'
+import db from "../firebase.js";
+import { collection, addDoc } from "firebase/firestore";
 
 const Transactions = () => {
+
+  
+
+  async function getTransactions(db) {
+    // access the collection from db, creates if not exists
+    const transactionsCol = collection(db, 'transactions');
+
+  }
+
+
+  const addTransaction = async () => {
+    await addDoc(collection(db, "transactions"), {
+      amount: 10000,
+      description: "Sample Amount",
+      date: new Date(),
+      type: "Debit",
+      category: "Fixed Expense"
+    });
+    alert("Transaction added Successfully");
+  }
+
+
   return (
     <div>
       <Transaction/>
-      <Transaction/>
-      <Transaction/>
-      <Transaction/>
-      <Transaction/>
-      <Transaction/>
-      <Transaction/>
-      <Transaction/>
-      <Transaction/>
+      <button className="p-2 rounded-lg ml-10 bg-green-600" onClick={addTransaction}>Add Transaction</button>
     </div>
   )
 }
