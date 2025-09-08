dc175afeb3fd034f7b72fa9693d0d26c744bf67d
Naman <namanuser1307@gmail.com>
2025-09-09 00:19:21 +0530

    Home Layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='grid grid-cols-6  grid-rows-5 gap-2 w-screen'>
+      <div className='col-span-3 row-span-2 bg-gray-500 rounded-lg'>Total expense of Month</div>
+      <div className='col-span-3 row-span-2 bg-gray-500 rounded-lg'>Last 5 transactions</div>
+      <div className='col-span-2 row-span-1 bg-blue-300 rounded-lg'>+ Add expense +</div>
+      <div className='col-span-2 row-span-1 bg-blue-300 rounded-lg'>+ Add transaction +</div>
+      <div className='col-span-2 row-span-1 bg-blue-300 rounded-lg'>Some Feature</div>
+      <div className='col-span-3 row-span-2 bg-gray-500 rounded-lg'>Daily expense of last 5 days</div>
+      <div className='col-span-3 row-span-2 bg-gray-500 rounded-lg'>Monthly expense of last 5 months</div>
+    </div>
   )
 }
 
