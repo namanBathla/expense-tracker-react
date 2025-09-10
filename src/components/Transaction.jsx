667994b1f4041d6c4287c4ece8e1cb6100a2a890
Naman <namanuser1307@gmail.com>
2025-09-11 01:48:51 +0530

    Fetch transactions from Firebase db

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const Transaction = () => {
+const Transaction = ({dateTime, amount, description, category, type}) => {
+
+  const colorClass = type === "credit" ? "text-green-600" : "text-red-600";
   return (
-    <div className='grid grid-cols-5 p-2 text-center'>
-      <div>05 Aug 2025</div>
-      <div>1000</div>
-      <div>Rent for the month of August</div>
-      <div>Fixed Expense</div>
+    <div className={`grid grid-cols-5 p-2 text-center ${colorClass}`}>
+      <div>{dateTime}</div>
+      <div>{amount}</div>
+      <div>{description}</div>
+      <div>{category}</div>
       <div className='text-red-600 cursor-pointer'>Delete</div>
     </div>
   )
--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -2,17 +2,41 @@ import React from 'react'
 import Transaction from '../components/Transaction'
 import db from "../firebase.js";
 import { collection, addDoc , getDocs} from "firebase/firestore";
+import { useState } from 'react';
 
 const Transactions = () => {
 
-  
+  const [transactions, setTransactions] = useState([]);
+
+
+  /* querySnapshot is not an array, it’s a QuerySnapshot object.
+  So you can’t do querySnapshot.map(...).
+  Instead, you need to map over its .docs array: */
+
   async function getTransactions() {
     // access the collection from db, creates if not exists
     const querySnapshot = await getDocs(collection(db, "transactions"));
-    querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
-  });
+    const data = querySnapshot.docs.map((doc) => {
+
+      // console.log(doc.data());
+      return {
+        id: doc.id,
+      dateTime: (new Date((doc.data().date.seconds)*1000)).toLocaleString(),
+      ...doc.data()
+      }
+      
+    });
+
+    setTransactions(data);
   }
+    /* this is same as 
+    data = querySnapshot.docs.map((doc) => {
+      return {id: doc.id, ....doc.data()}
+    }                                             */
+
+    /* Wrapping in () tells JavaScript: “This is a single expression, specifically an object literal.”
+  So it correctly returns the object from the arrow function. */
+      // transactions.push(doc.data());
 
 
   const addTransaction = async () => {
@@ -29,7 +53,15 @@ const Transactions = () => {
 
   return (
     <div>
-      <Transaction/>
+
+      <div className='flex flex-col gap-4'>
+        {transactions.map((transaction => {
+        return <Transaction key={transaction.id} {...transaction}/>
+      }))}
+
+      </div>
+      {/* <Transaction/> */}
+      
       <button className="p-2 rounded-lg ml-10 bg-green-600" onClick={addTransaction}>Add Transaction</button>
       <button className="p-2 rounded-lg ml-10 bg-green-600" onClick={getTransactions}>Get Trs</button>
       
