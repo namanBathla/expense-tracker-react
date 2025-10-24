b899b8ce88c8488bd1977784cd04d3e7385b782c
Naman <namanuser1307@gmail.com>
2025-10-24 15:22:40 +0530

    Add icons to NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { MdAddCircle, MdAttachMoney, MdCreditCard, MdDashboard, MdDescription, MdHome } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
@@ -10,15 +11,36 @@ const Navbar = () => {
   ? "bg-slate-700 px-3 py-2 rounded-md text-white transition-colors duration-300"
   : "text-white px-3 py-2 transition-colors duration-300";
   }
-
+  const links = [
+    {name: "HOME", path:"/", icon:MdHome},
+    {name: "DASHBOARD", path:"/dashboard", icon: MdDashboard},
+    {name: "TRANSACTIONS", path:"/transactions", icon: MdCreditCard},
+    {name: "EXPENSES", path:"/expenses", icon: MdAttachMoney},
+    {name: "REPORTS", path:"/reports", icon: MdDescription},
+    {name: "ADD", path:"/add", icon: MdAddCircle},
+  ]
   return (<>
-    <div className="fixed left-0 top-0 flex flex-col justify-center items-center gap-5 bg-slate-800 text-white h-screen px-5">
+    {/* <div className="fixed left-0 top-0 flex flex-col justify-center items-center gap-5 bg-slate-800 text-white h-screen px-5">
       <NavLink to="/" className={onActiveLink}>HOME</NavLink>
       <NavLink to="/dashboard" className={onActiveLink}>DASHBOARD</NavLink>
       <NavLink to="/transactions" className={onActiveLink}>TRANSACTIONS</NavLink>
       <NavLink to="/reports" className={onActiveLink}>REPORTS</NavLink>
       <NavLink to="/expenses" className={onActiveLink}>EXPENSES</NavLink>
       <NavLink to="/add" className={onActiveLink}>ADD</NavLink>
+    </div> */}
+
+    <div className="fixed left-0 top-0 flex flex-col justify-center items-center gap-5 bg-slate-800 text-white h-screen px-5">
+      {links.map(link => {
+        const Icon = link.icon;
+        return (
+          <NavLink key={link.name} to={link.path} className={onActiveLink}>
+            <div className="flex">
+              <Icon className="h-5 w-5 mr-2"/>
+              {link.name}
+            </div>
+          </NavLink>
+        );
+      })}
     </div>
   </>);
 };
